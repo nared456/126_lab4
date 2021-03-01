ced44e61cec9a963b1763f04de21e7f97f47ba18
nared456 <78350512+nared456@users.noreply.github.com>
2021-03-01 17:17:54 +0700

    Acronym.c_6320503041

---
+++ Acronym.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main()
+{
+    int i = 0, j, k = 0;
+    char word[1000];
+    char delim[10][5] = {"i", "of", "at", "with", "for", "the", "on", "a", "an", "in"};
+    char *p;
+    gets(word);
+    p = strtok(word," ");
+    while (p != '\0')
+    {
+        for (j=0 ; j < 10; j++)
+        {
+            if (strcmp(p, delim[j]))
+                k++;
+        }
+        if (k == 10 || i == 0)
+        {
+            printf("%c", toupper(p[0]));
+        }
+        p = strtok(NULL," ");
+        i++, k = 0;
+    }
+    return 0;
+}
